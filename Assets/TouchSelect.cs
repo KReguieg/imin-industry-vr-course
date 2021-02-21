9f69706cc81d29bb1103aa1aad39963adb305764
Khaled Reguieg <Khaled.Reguieg@island-labs.com>
2021-02-21 19:12:24 +0100

    Initial commit with lab scene

---
+++ Assets/TouchSelect.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRSimpleInteractable))]
+public class TouchSelect : MonoBehaviour
+{
+    XRSimpleInteractable m_SimpleInteractable;
+    MeshRenderer m_MeshRenderer;
+
+    static Color s_UnityMagenta = new Color(0.929f, 0.094f, 0.278f);
+    public Color s_UnityCyan = new Color(0.019f, 0.733f, 0.827f);
+
+    bool m_Held;
+
+    protected void OnEnable()
+    {
+        m_SimpleInteractable = GetComponent<XRSimpleInteractable>();
+        m_MeshRenderer = GetComponent<MeshRenderer>();
+
+        m_SimpleInteractable.hoverEntered.AddListener(OnHoverEntered);
+    }
+
+    private void OnHoverEntered(HoverEnterEventArgs arg0)
+    {
+        Debug.Log(arg0.interactor.name);
+        m_MeshRenderer.material.color = s_UnityCyan;
+    }
+
+    protected void OnDisable()
+    {
+        m_SimpleInteractable.firstHoverEntered.RemoveListener(OnHoverEntered);
+    }
+
+    protected virtual void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        m_MeshRenderer.material.color = s_UnityCyan;
+        m_Held = true;
+    }
+
+    protected virtual void OnSelectExited(SelectExitEventArgs args)
+    {
+        m_MeshRenderer.material.color = Color.white;
+        m_Held = false;
+    }
+
+    protected virtual void OnLastHoverExited(HoverExitEventArgs args)
+    {
+        if (!m_Held)
+        {
+            m_MeshRenderer.material.color = Color.white;
+        }
+    }
+
+    protected virtual void OnFirstHoverEntered(HoverEnterEventArgs args)
+    {
+        if (!m_Held)
+        {
+            m_MeshRenderer.material.color = s_UnityMagenta;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # imin-industry-vr-course
-This repository holds the immersive insiders course where students learn how to create a industry, training-like VR course. Including the new Unity XR interaction toolkit, device simulator and dope 3D models crafted by the @digitalanimals
+
+This repository holds the immersive insiders course where students learn how to create a industry, training-like VR course.
+
+## Road Map
+
+The course is including:
+* [x] The new Unity XR interaction toolkit
+* [x] Device Simulator
+* [x] Locomotion
+* [x] Custom Hands
+* [x] Hand Interaction
+* [x] Custom coded interactions
+* [x] Puzzles
+* [x] Snapping Helpers
+* [x] and dope 3D models crafted by the @digitalanimals
