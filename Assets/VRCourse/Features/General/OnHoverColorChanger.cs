9afb491ff54c1f04d279cacf9772671f7e14a09f
Khaled Reguieg <Khaled.Reguieg@island-labs.com>
2021-02-23 10:19:44 +0100

    Add PushButton working vertically and horizontally :rocket:

---
+++ Assets/VRCourse/Features/General/OnHoverColorChanger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRSimpleInteractable))]
+public class OnHoverColorChanger : MonoBehaviour
+{
+    private XRSimpleInteractable simpleInteractable = null;
+    private MeshRenderer meshRenderer = null;
+
+    private Color startColor = Color.white;
+    public Color hoverColor = new Color(0.019f, 0.733f, 0.827f);
+
+    protected void OnEnable()
+    {
+        simpleInteractable = GetComponent<XRSimpleInteractable>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        startColor = meshRenderer.material.color;
+
+        simpleInteractable.hoverEntered.AddListener(OnHoverEntered);
+        simpleInteractable.hoverExited.AddListener(OnHoverExited);
+    }
+
+    private void OnHoverExited(HoverExitEventArgs arg0)
+    {
+        meshRenderer.material.color = startColor;
+    }
+
+    private void OnHoverEntered(HoverEnterEventArgs arg0)
+    {
+        meshRenderer.material.color = hoverColor;
+    }
+
+    protected void OnDisable()
+    {
+        simpleInteractable.hoverEntered.RemoveListener(OnHoverEntered);
+        simpleInteractable.hoverExited.RemoveListener(OnHoverExited);
+    }
+
+}
