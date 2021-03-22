5e08357cda42992a7b34a203c2fe939cadcf8eab
Dinesh Punni <dinesh.punni@island-labs.com>
2021-03-22 17:06:50 +0100

    Added GazeInteraction

---
+++ Assets/VRGazeInteraction/CoreFramework/AnimationFramework/Scripts/MoveAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveAnimation : MonoBehaviour, AnimationBase
+{
+
+    public MoveProfile MoveProfile;
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.localPosition;
+    }
+
+    public void Animate(bool value)
+    {
+        var position = value ? MoveProfile.moveTo : startPos;
+        transform.DOMove(position, MoveProfile.speed);      
+    }
+
+}
+
