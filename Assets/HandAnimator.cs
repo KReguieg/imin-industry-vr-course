1fc2bc44636f5b0116946679dd23e06cea667f19
Khaled Reguieg <Khaled.Reguieg@island-labs.com>
2021-03-29 23:42:08 +0200

    [ADD] hand animation via input action system

---
+++ Assets/HandAnimator.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof (Animator))]
+public class HandAnimator : MonoBehaviour
+{
+    [SerializeField] private InputActionReference controllerActionGrip;
+    [SerializeField] private InputActionReference controllerActionTrigger;
+    public GameObject directionLight;
+    public InputAction grip;
+    public InputAction trigger;
+    private Animator handAnimator = null;
+    private float animationSpeed = 5.0f;
+
+    /// <summary>
+    /// List of fingers animated when grabbing / using grab action
+    /// </summary>
+    private readonly List<Finger> grippingFingers = new List<Finger>()
+    {
+        new Finger(FingerType.Middle),
+        new Finger(FingerType.Ring),
+        new Finger(FingerType.Pinky)
+    };
+
+    /// <summary>
+    /// List of fingers animated when pointing / using trigger action
+    /// </summary>
+    private readonly List<Finger> pointingFingers = new List<Finger>()
+    {
+        new Finger(FingerType.Thumb),
+        new Finger(FingerType.Index)
+    };
+
+    /// <summary>
+    /// Add your own hand animation here. For example a fist or a peace sign.
+    /// </summary>
+    //private readonly List<Finger> Fingers = new List<Finger>()
+    //{
+    //    new Finger(FingerType.Index)
+    //};
+
+    private void OnEnable()
+    {
+        // Have it run your code when the Action is triggered.
+        controllerActionGrip.action.performed += _ => PerformGripAnimation();
+        controllerActionTrigger.action.performed += _ => PerformTriggerAnimation();
+
+        controllerActionGrip.action.canceled += GripAction_canceled;
+        controllerActionTrigger.action.canceled += TriggerAction_canceled;
+    }
+
+    private void TriggerAction_canceled(InputAction.CallbackContext obj)
+    {
+        SetFingerAnimationValues(pointingFingers, 0.0f);
+        AnimateActionInput(pointingFingers);
+    }
+
+    private void GripAction_canceled(InputAction.CallbackContext obj)
+    {
+        SetFingerAnimationValues(grippingFingers, 0.0f);
+        AnimateActionInput(grippingFingers);
+    }
+
+    private void PerformGripAnimation()
+    {
+        SetFingerAnimationValues(grippingFingers, 1.0f);
+        AnimateActionInput(grippingFingers);
+    }
+
+    private void PerformTriggerAnimation()
+    {
+        SetFingerAnimationValues(pointingFingers, 1.0f);
+        AnimateActionInput(pointingFingers);
+    }
+
+
+    private void Awake()
+    {
+        this.handAnimator = GetComponent<Animator>();
+    }
+
+    private void AnimateActionInput(List<Finger> fingersToAnimate)
+    {
+        foreach (Finger finger in fingersToAnimate)
+        {
+            AnimateFinger(finger.GetType().ToString(), finger.current);
+        }
+    }
+
+    private void AnimateFinger(string fingerName, float animationBlendValue)
+    {
+        handAnimator.SetFloat(fingerName, animationBlendValue);
+    }
+
+    private void CheckForTriggerInput()
+    {
+        //if (controller.inputDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.trigger, out float pointingValue))
+        //    SetFingerAnimationValues(pointingFingers, pointingValue);
+    }
+
+    private void CheckForGripInput()
+    {
+        //if (controller.inputDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out float gripValue))
+        //    SetFingerAnimationValues(pointingFingers, gripValue);
+    }
+
+    private void SetFingerAnimationValues(List<Finger> fingersToAnimate, float targetValue)
+    {
+        foreach (Finger finger in fingersToAnimate)
+        {
+            finger.target = targetValue;
+        }
+    }
+
+    private void SmoothFingerAnimation(List<Finger> fingersToSmooth)
+    {
+        foreach (Finger finger in fingersToSmooth)
+        {
+            float animationTimeStep = animationSpeed * Time.unscaledDeltaTime;
+            finger.current = Mathf.MoveTowards(finger.current, finger.target, animationTimeStep);
+        }
+    }
+
+    private void OnDisable()
+    {
+        controllerActionGrip.action.performed -= _ => PerformGripAnimation();
+        controllerActionTrigger.action.performed -= _ => PerformTriggerAnimation();
+
+        controllerActionGrip.action.canceled -= GripAction_canceled;
+        controllerActionTrigger.action.canceled -= TriggerAction_canceled;
+    }
+}
