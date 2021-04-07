1698142b17189c58fddeef84667d74d4370c3648
Dinesh Punni <dinesh.punni@island-labs.com>
2021-04-07 21:57:14 +0200

    Finalized GazeLock system

--- Assets/LockController.cs
+++ Assets/LockController.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
+[RequireComponent(typeof(AudioSource))]
 public class LockController : MonoBehaviour
 {
+
+    public List<TMP_Text> numbersText = new List<TMP_Text>();
+
+    public float numberSpeed;
+
+    AudioSource audioSource;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -14,13 +23,35 @@ public class LockController : MonoBehaviour
         
     }
 
-    public void AddOne(int index)
+    public void StartAdding(int index)
     {
-        
+        StartCoroutine(StartCountingRoutine(index, 1));
     }
 
-    public void SubtractOne(int index)
+    public void StartSubtracking(int index)
     {
+        StartCoroutine(StartCountingRoutine(index, -1));
 
+    }
+
+    public void StopCounting()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator StartCountingRoutine(int index, int amount)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(numberSpeed);
+
+            int newNumber = amount + int.Parse(numbersText[index].text);
+
+            newNumber = newNumber > 9 ? 0 : newNumber;
+            newNumber = newNumber < 0 ? 9 : newNumber;
+
+            numbersText[index].text = newNumber.ToString();
+            audioSource.Play();
+        }
     }
 }
